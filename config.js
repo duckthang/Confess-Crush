dfe4f86aa703e3caf442bc32ed9ba6b0ac71e040
duckthang <106187994+duckthang@users.noreply.github.com>
2022-05-25 15:37:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "hiiii",
+    introTitle: 'Chào Quân',
+    introDesc: `Thắng pro víp nhất vụ trụ đây
+    `,
+    btnIntro: '^^ Gớm không^^',
+    title: 'Ai đi mid gớm nhất Nghệ An ',
+    btnYes: 'Thắng chơ ai',
+    btnNo: 'Thắng non',
+    question: 'Một từ nhận xét về trình độ của Thắng',
+    btnReply: 'nhận xét',
+    reply: 'quá pro',
+    mess: 'quá rọ ràng',
+    messDesc: 'tối chơi game k.',
+    btnAccept: 'ok',
+    messLink: 'https://www.facebook.com/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
